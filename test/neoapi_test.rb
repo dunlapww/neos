f402f0c83c3bdd992194fec71b34917182402690
Will Dunlap <dunlapww@gmail.com>
2020-11-20 15:50:15 -0700

    Add test it has date

--- test/neoapi_test.rb
+++ test/neoapi_test.rb
@@ -5,7 +5,14 @@ require_relative '../lib/neoapi'
 
 class NeoapiTest < Minitest::Test
   def test_it_exists
-    api_pull = Neoapi.new
+    date = "2019-03-30"
+    api_pull = Neoapi.new(date)
     assert_instance_of Neoapi, api_pull
+  end
+  
+  def test_it_has_date
+    date = "2019-03-30"
+    neoapi = Neoapi.new(date)
+    assert_equal("2019-03-30", neoapi.date)
   end
 end
