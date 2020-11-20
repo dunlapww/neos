34ea1d1f9ab2a04e25e1233a8a23803e7c5e069e
Will Dunlap <dunlapww@gmail.com>
2020-11-20 15:36:25 -0700

    Test Asteroid exists

---
+++ test/asteroid_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require_relative '../lib/asteroid'
+
+class AsteroidTest < Minitest::Test
+  def test_it_exists
+    asteroid_details = Hash.new
+    asteroid_details[:name] = "Larry"
+    asteroid_details[:diameter] = 10
+    asteroid_details[:miss_distance] = 30
+    asteroid = Asteroid.new(asteroid_details)
+    assert_instance_of Asteroid, asteroid
+  end
+end
