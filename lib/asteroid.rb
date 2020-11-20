5fe5fc7ef7ed3f43bf7dfe013bd77f563265404f
Will Dunlap <dunlapww@gmail.com>
2020-11-20 15:36:46 -0700

    Pass Asteroid exists

---
+++ lib/asteroid.rb
@@ -0,0 +1,9 @@
+class Asteroid
+
+  def initialize(asteroid_details)
+    name = asteroid_details[:name]
+    diameter = asteroid_details[:diameter]
+    miss_distance = asteroid_details[:miss_distance]
+  end
+
+end
