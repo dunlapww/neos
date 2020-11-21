2fdf640c423589f893b1a8539756481fb8430951
Will Dunlap <dunlapww@gmail.com>
2020-11-20 17:55:43 -0700

    Pass test for largest and count of asteroids

--- asteroid.rb
+++ asteroid.rb
@@ -7,4 +7,18 @@ class Asteroid
     @miss_distance = asteroid_details[:close_approach_data][0][:miss_distance][:miles].to_i
   end
 
+  def self.all_asteroids(asteroid_data)
+    asteroid_data.map do |asteroid_details|
+      Asteroid.new(asteroid_details)
+    end
+  end
+
+  def self.count(asteroid_data)
+    all_asteroids(asteroid_data).size
+  end
+
+  def self.largest(asteroid_data)
+    all_asteroids(asteroid_data).max_by{|asteroid| asteroid.diameter}
+  end
+
 end
