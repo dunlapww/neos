2138b2861ccae5a26a8f74c4c0579b1d17c2f12c
Will Dunlap <dunlapww@gmail.com>
2020-11-20 17:30:53 -0700

    Pass Asteroid has details

--- asteroid.rb
+++ asteroid.rb
@@ -1,9 +1,10 @@
 class Asteroid
+  attr_reader :name, :diameter, :miss_distance
 
   def initialize(asteroid_details)
-    name = asteroid_details[:name]
-    diameter = asteroid_details[:diameter]
-    miss_distance = asteroid_details[:miss_distance]
+    @name = asteroid_details[:name]
+    @diameter = asteroid_details[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+    @miss_distance = asteroid_details[:close_approach_data][0][:miss_distance][:miles].to_i
   end
 
 end
